61d58ca3c33eba3529a768e538dd13b691187185
faxapcard <99022586+faxapcard@users.noreply.github.com>
2022-02-04 17:55:48 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,18 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "HaishinKit",
-    platforms: [
-        .macOS(.v10_15),
-    ],
+    
     products: [
         .library(name: "HaishinKit", targets: ["HaishinKit"]),
     ],
     dependencies: [
         .package(url: "https://github.com/shogo4405/Logboard.git", from: "1.0.0"),
-    ],
-    targets: [
-        .target(
-            name: "HaishinKit",
-            dependencies: ["Logboard"])
     ]
+    
 )
