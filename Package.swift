a2d20223cd0a6c36b7b51b6d455aeca100769149
faxapcard <99022586+faxapcard@users.noreply.github.com>
2022-02-04 17:57:10 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,6 +10,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/shogo4405/Logboard.git", from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target defines a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "HaishinKit",
+            dependencies: [])
     ]
     
 )
